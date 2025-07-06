4f0a7ff4542678f4f64ad24c509dde96ab745a30
Nayeli <champ_nay02@hotmail.com>
2025-07-06 01:41:16 -0500

    Add new carousel images and update Carousel component
    Added three new banner images for the carousel and updated Carousel.jsx to display all four images with a 2500ms interval. Also removed unused global CSS styles for headers, links, and buttons from App.css.

--- src/main/java/com/example/Reyna/view/components/Carousel.jsx
+++ src/main/java/com/example/Reyna/view/components/Carousel.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import bannerPerfume from '../../../../../../../assets/img/banner-perfume.png';
+import bannerPerfume1 from '../../../../../../../assets/img/banner-perfume.png';
+import bannerPerfume2 from '../../../../../../../assets/img/banner-perfume2.png';
+import bannerPerfume3 from '../../../../../../../assets/img/banner-perfume3.png';
+import bannerPerfume4 from '../../../../../../../assets/img/banner-perfume4.png';
 
 const Carousel = () => {
   return (
-    <section id="banner-principal" className="carousel slide" data-bs-ride="carousel">
+    <section id="banner-principal" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2500">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src={bannerPerfume} className="d-block w-100" alt="Banner Perfume" />
+          <img src={bannerPerfume1} className="d-block w-100" alt="Banner Perfume 1" />
+        </div>
+        <div className="carousel-item">
+          <img src={bannerPerfume2} className="d-block w-100" alt="Banner Perfume 2" />
+        </div>
+        <div className="carousel-item">
+          <img src={bannerPerfume3} className="d-block w-100" alt="Banner Perfume 3" />
+        </div>
+        <div className="carousel-item">
+          <img src={bannerPerfume4} className="d-block w-100" alt="Banner Perfume 4" />
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#banner-principal" data-bs-slide="prev">
